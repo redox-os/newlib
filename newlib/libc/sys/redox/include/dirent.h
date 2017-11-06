b2430061aae1eef29f78f352ee6f5fc9ed6f66f9
Paul Sajna <paulsajna@gmail.com>
2017-11-06 08:14:27 -0800

    edit dirent.h and sys/dirent.h

--- newlib/libc/sys/redox/include/dirent.h
+++ newlib/libc/sys/redox/include/dirent.h
@@ -14,16 +14,6 @@ extern "C" {
 }
 #endif
 
-__BEGIN_DECLS
-int closedir(DIR *);
-DIR *opendir(const char *);
-struct dirent *readdir(DIR *);
-void rewinddir(DIR *);
-int scandir(const char *dirp, struct dirent ***namelist,
-              int (*filter)(const struct dirent *),
-              int (*compar)(const struct dirent **, const struct dirent **));
-int alphasort(const struct dirent **d1, const struct dirent **d2);
-__END_DECLS
 
 #endif /*_DIRENT_H_*/
 
